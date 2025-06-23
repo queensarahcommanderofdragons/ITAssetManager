68377f440daf608cc932b3efce053c8834a99e07
queensarahcommanderofdragons <jayneS.jobling@gmail.com>
2025-06-23 10:39:10 +0100

    Fix loaddata input for db_utf16.json

--- ITAssetManager/views.py
+++ ITAssetManager/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.core.management import call_command
 from django.http import HttpResponse
+import io
 
 def home(request):
     return render(request, "home.html")
 
 def load_db_view(request):
     try:
-        call_command('loaddata', 'db.json')
+        with open('db_utf16.json', 'r', encoding='utf-16') as f:
+            data = f.read()
+        stream = io.StringIO(data)
+        call_command('loaddata', stream)
         return HttpResponse("Data loaded successfully into PostgreSQL.")
     except Exception as e:
         return HttpResponse(f" Error: {e}")
