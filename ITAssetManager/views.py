58b7f3f2be683d702adae48d2d161fd8999effa4
queensarahcommanderofdragons <jayneS.jobling@gmail.com>
2025-06-23 11:09:19 +0100

    Fix: UTF-16 converted to UTF-8 to help render

--- ITAssetManager/views.py
+++ ITAssetManager/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.core.management import call_command
 from django.http import HttpResponse
-import io
 
 def home(request):
     return render(request, "home.html")
@@ -9,13 +8,7 @@ def home(request):
 
 def load_db_view(request):
     try:
-        with open('db_utf16.json', 'rb') as f:
-            raw_data = f.read()
-        decoded_data = raw_data.decode('utf-16')
-        stream = io.StringIO(decoded_data)
-        stream.name = 'db_utf16.json'  # Django uses this to infer format
-        call_command('loaddata', stream)
+        call_command('loaddata', 'db.json')
         return HttpResponse(" Data loaded successfully into PostgreSQL.")
     except Exception as e:
         return HttpResponse(f" Error: {e}")
-
