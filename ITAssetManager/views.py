e577daa8fcc2121cfc2dda4d86c0f80724feba28
queensarahcommanderofdragons <jayneS.jobling@gmail.com>
2025-06-23 10:58:22 +0100

    Fix: load db_utf16.json diretlu wiht format=json

--- ITAssetManager/views.py
+++ ITAssetManager/views.py
@@ -8,11 +8,7 @@ def home(request):
 
 def load_db_view(request):
     try:
-        with open('db_utf16.json', 'r', encoding='utf-16') as f:
-            data = f.read()
-        stream = io.StringIO(data)
-        stream.name = 'db_utf16.json'
-        call_command('loaddata', stream)
-        return HttpResponse("Data loaded successfully into PostgreSQL.")
+        call_command('loaddata', 'db_utf16.json', format='json')
+        return HttpResponse("✅ Data loaded successfully into PostgreSQL.")
     except Exception as e:
-        return HttpResponse(f" Error: {e}")
+        return HttpResponse(f"❌ Error: {e}")
